110fe40ec57018c7c3da5ae214b8e9bb9881dc46
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air-2.local>
2023-07-05 23:10:28 +0200

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -76,12 +76,29 @@ impl NetflowParser {
         match NetflowHeader::parse_be(packet) {
             Ok((_, netflow_header)) if netflow_header.version == 5 => V5::parse_bytes(packet),
             Ok((_, netflow_header)) if netflow_header.version == 7 => V7::parse_bytes(packet),
-            _ => Err(format!("Not Supported").into()),
+            _ => Err("Not Supported".to_string().into()),
         }
     }
 
     /// Takes a Netflow packet slice and returns a vector of Parsed Netflows.
     /// If we reach some parse error we return what items be have.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use serde_json::json;
+    /// use netflow_parser::NetflowParser;
+    ///
+    /// let v5_packet = [0, 5, 2, 0, 3, 0, 4, 0, 5, 0, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7,];
+    /// println!("{}", json!(NetflowParser::parse_bytes(&v5_packet)).to_string());
+    /// ```
+    ///
+    /// ## Output:
+    ///
+    /// ```json
+    /// [{"V5":{"body":{"d_octets":66051,"d_pkts":101124105,"dst_addr":"4.5.6.7","dst_as":515,"dst_mask":5,"dst_port":1029,"first":67438087,"input":515,"last":134807553,"next_hop":"8.9.0.1","output":1029,"pad1":6,"pad2":1543,"protocol":"EGP","src_addr":"0.1.2.3","src_as":1,"src_mask":4,"src_port":515,"tcp_flags":7,"tos":9},"header":{"count":512,"engine_id":7,"engine_type":6,"flow_sequence":33752069,"sampling_interval":2057,"sys_up_time":50332672,"unix_nsecs":134807553,"unix_secs":83887623,"unix_time":{"nanos_since_epoch":134807553,"secs_since_epoch":83887623},"version":5}}}]
+    /// ```
+    ///
     pub fn parse_bytes(packet: &[u8]) -> Vec<NetflowPacket> {
         let mut packet_to_be_processed = <&[u8]>::clone(&packet);
         let mut netflow_results = vec![];
