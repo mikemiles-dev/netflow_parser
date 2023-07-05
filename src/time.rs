bfb5c348c52573468e9d6e09f5985b4c63895fba
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air-2.local>
2023-07-05 23:13:42 +0200

    doc

--- src/time.rs
+++ src/time.rs
@@ -1,6 +1,7 @@
 use std::time::{Duration, SystemTime};
 
-pub fn build_unix_time(secs: u32, n_secs: u32) -> SystemTime {
+/// Builda a System time from unix_secd and unix_nsecs usually provided by a Netflow Header
+pub(crate) fn build_unix_time(secs: u32, n_secs: u32) -> SystemTime {
     SystemTime::UNIX_EPOCH
         + Duration::from_secs(secs.into())
         + Duration::from_nanos(n_secs.into())
