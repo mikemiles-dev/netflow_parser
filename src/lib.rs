474f276bbe844e02e6878eb2e41dcd1f1f2b5eb9
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air-2.local>
2023-07-11 00:41:48 +0200

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 //! # netflow_parser
 //!
-//! # Description:
 //! A netflow_parser library for V5, V7, V9, IPFIX written in Rust (More Versions coming soon).
 //! Supports chaining of multple versions in the same stream.  (<v5 packet>, <v7packet>, <v5packet>, <v9packet>, etc.)
 //!
