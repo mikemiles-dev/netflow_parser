efdef6b92037df32f8c8b9495d8b49e8ae7d5324
Michael Mileusnich <mmileusnich@gmail.com>
2024-03-19 10:24:44 -0500

    Lib cleanup

---
+++ src/netflow_header.rs
@@ -0,0 +1,38 @@
+use nom::number::complete::be_u16;
+use nom::IResult;
+use nom_derive::{Nom, Parse};
+
+/// Struct is used simply to match how to handle the result of the packet
+#[derive(Nom)]
+pub struct NetflowHeader {
+    /// Netflow Version
+    #[nom(Map = "NetflowVersion::from", Parse = "be_u16")]
+    pub version: NetflowVersion,
+}
+
+impl NetflowHeader {
+    pub fn parse_header(packet: &[u8]) -> IResult<&[u8], NetflowHeader> {
+        NetflowHeader::parse_be(packet)
+    }
+}
+
+#[derive(PartialEq)]
+pub enum NetflowVersion {
+    V5,
+    V7,
+    V9,
+    IPFix,
+    Unsupported,
+}
+
+impl From<u16> for NetflowVersion {
+    fn from(version: u16) -> Self {
+        match version {
+            5 => NetflowVersion::V5,
+            7 => NetflowVersion::V7,
+            9 => NetflowVersion::V9,
+            10 => NetflowVersion::IPFix,
+            _ => NetflowVersion::Unsupported,
+        }
+    }
+}
