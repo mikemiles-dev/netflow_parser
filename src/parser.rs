1e0e375d6ae977d274232a29d8cc06b0177bb9c1
Michael Mileusnich <mmileusnich@gmail.com>
2024-03-21 16:10:23 -0500

    Cleanup

---
+++ src/parser.rs
@@ -0,0 +1,158 @@
+use nom::number::complete::be_u16;
+use nom_derive::{Nom, Parse};
+
+use crate::static_versions::{v5::V5, v7::V7};
+use crate::variable_versions::ipfix::IPFix;
+use crate::variable_versions::v9::V9;
+use crate::{NetflowPacketResult, NetflowParser, ParsedNetflow};
+
+/// Struct is used simply to match how to handle the result of the packet
+#[derive(Nom)]
+pub struct NetflowHeader {
+    /// Netflow Version
+    #[nom(Map = "NetflowVersion::from", Parse = "be_u16")]
+    pub version: NetflowVersion,
+}
+
+pub enum NetflowHeaderResult<'a> {
+    V5(&'a [u8]),
+    V7(&'a [u8]),
+    V9(&'a [u8]),
+    IPFix(&'a [u8]),
+}
+
+impl NetflowHeader {
+    pub fn parse_header(
+        packet: &[u8],
+    ) -> Result<NetflowHeaderResult, Box<dyn std::error::Error>> {
+        match NetflowHeader::parse_be(packet) {
+            Ok((i, header)) if header.version.is_v5() => Ok(NetflowHeaderResult::V5(i)),
+            Ok((i, header)) if header.version.is_v7() => Ok(NetflowHeaderResult::V7(i)),
+            Ok((i, header)) if header.version.is_v9() => Ok(NetflowHeaderResult::V9(i)),
+            Ok((i, header)) if header.version.is_ipfix() => Ok(NetflowHeaderResult::IPFix(i)),
+            _ => Err(("Unsupported Version").into()),
+        }
+    }
+}
+
+#[derive(PartialEq)]
+pub enum NetflowVersion {
+    V5,
+    V7,
+    V9,
+    IPFix,
+    Unsupported,
+}
+
+impl NetflowVersion {
+    pub fn is_v5(&self) -> bool {
+        *self == NetflowVersion::V5
+    }
+    pub fn is_v7(&self) -> bool {
+        *self == NetflowVersion::V7
+    }
+    pub fn is_v9(&self) -> bool {
+        *self == NetflowVersion::V9
+    }
+    pub fn is_ipfix(&self) -> bool {
+        *self == NetflowVersion::IPFix
+    }
+}
+
+impl From<u16> for NetflowVersion {
+    fn from(version: u16) -> Self {
+        match version {
+            5 => NetflowVersion::V5,
+            7 => NetflowVersion::V7,
+            9 => NetflowVersion::V9,
+            10 => NetflowVersion::IPFix,
+            _ => NetflowVersion::Unsupported,
+        }
+    }
+}
+
+impl From<V5> for NetflowPacketResult {
+    fn from(v5: V5) -> Self {
+        Self::V5(v5)
+    }
+}
+
+impl From<V7> for NetflowPacketResult {
+    fn from(v7: V7) -> Self {
+        Self::V7(v7)
+    }
+}
+
+impl From<V9> for NetflowPacketResult {
+    fn from(v9: V9) -> Self {
+        Self::V9(v9)
+    }
+}
+
+impl From<IPFix> for NetflowPacketResult {
+    fn from(ipfix: IPFix) -> Self {
+        Self::IPFix(ipfix)
+    }
+}
+
+pub type V5ParsedResult<'a> = (&'a [u8], V5);
+pub type V7ParsedResult<'a> = (&'a [u8], V7);
+pub type V9ParsedResult<'a> = (&'a [u8], V9);
+pub type IPFixParsedResult<'a> = (&'a [u8], IPFix);
+
+impl<'a> From<V5ParsedResult<'a>> for ParsedNetflow {
+    fn from((remaining, v5_parsed): V5ParsedResult) -> ParsedNetflow {
+        Self::new(remaining, v5_parsed.into())
+    }
+}
+
+impl<'a> From<V7ParsedResult<'a>> for ParsedNetflow {
+    fn from((remaining, v7_parsed): V7ParsedResult) -> ParsedNetflow {
+        Self::new(remaining, v7_parsed.into())
+    }
+}
+
+impl<'a> From<V9ParsedResult<'a>> for ParsedNetflow {
+    fn from((remaining, v9_parsed): V9ParsedResult) -> ParsedNetflow {
+        Self::new(remaining, v9_parsed.into())
+    }
+}
+
+impl<'a> From<IPFixParsedResult<'a>> for ParsedNetflow {
+    fn from((remaining, ipfix_parsed): IPFixParsedResult) -> ParsedNetflow {
+        Self::new(remaining, ipfix_parsed.into())
+    }
+}
+
+pub trait Parser {
+    fn parse<'a>(
+        &'a mut self,
+        packet: &'a [u8],
+    ) -> Result<ParsedNetflow, Box<dyn std::error::Error>>;
+}
+
+impl Parser for NetflowParser {
+    /// Parses a Netflow by version packet and returns a Parsed Netflow.
+    fn parse<'a>(
+        &'a mut self,
+        packet: &'a [u8],
+    ) -> Result<ParsedNetflow, Box<dyn std::error::Error>> {
+        match NetflowHeader::parse_header(packet) {
+            Ok(NetflowHeaderResult::V5(v5_packet)) => V5::parse(v5_packet)
+                .map(|r: V5ParsedResult| r.into())
+                .map_err(|e| format!("Could not parse V5 packet: {e}").into()),
+            Ok(NetflowHeaderResult::V7(v7_packet)) => V7::parse(v7_packet)
+                .map(|r: V7ParsedResult| r.into())
+                .map_err(|e| format!("Could not parse V7 packet: {e}").into()),
+            Ok(NetflowHeaderResult::V9(v9_packet)) => V9::parse(v9_packet, &mut self.v9_parser)
+                .map(|r: V9ParsedResult| r.into())
+                .map_err(|e| format!("Could not parse V9 packet: {e}").into()),
+            Ok(NetflowHeaderResult::IPFix(ipfix_packet)) => {
+                IPFix::parse(ipfix_packet, &mut self.ipfix_parser)
+                    .map(|r: IPFixParsedResult| r.into())
+                    .map_err(|e| format!("Could not parse v10 packet: {e}").into())
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
---
+++ src/tests.rs
@@ -0,0 +1,243 @@
+#[cfg(test)]
+mod tests {
+
+    use crate::variable_versions::ipfix::{
+        Template as IPFixTemplate, TemplateField as IPFixTemplateField,
+    };
+    use crate::variable_versions::v9::{
+        Template as V9Template, TemplateField as V9TemplateField,
+    };
+    use crate::NetflowParser;
+    use insta::assert_yaml_snapshot;
+
+    #[test]
+    fn it_parses_unix_timestamp_correctly() {
+        use nom::number::complete::{be_u32, be_u64};
+        use std::time::Duration;
+
+        let packet = [5, 0, 6, 7, 8, 9, 0, 1];
+        let (remain, secs1) =
+            be_u32::<&[u8], nom::error::Error<&[u8]>>(packet.as_slice()).unwrap();
+        let (remain, nsecs1) = be_u32::<&[u8], nom::error::Error<&[u8]>>(remain).unwrap();
+        assert_eq!(remain, []);
+
+        let time1 = Duration::from_nanos(nsecs1 as u64) + Duration::from_secs(secs1 as u64);
+
+        let (remain, secs_nsecs) =
+            be_u64::<&[u8], nom::error::Error<&[u8]>>(packet.as_slice()).unwrap();
+        assert_eq!(remain, []);
+        let secs2 = (secs_nsecs >> 32) as u32 as u64;
+        let nsecs2 = secs_nsecs as u32;
+
+        let time2 = Duration::new(secs2, nsecs2);
+
+        assert_eq!(secs1 as u64, secs2);
+        assert_eq!(nsecs1, nsecs2);
+        assert_eq!(time1, time2);
+    }
+
+    #[test]
+    #[cfg(not(feature = "unix_timestamp"))]
+    fn it_parses_v5() {
+        let packet = [
+            0, 5, 2, 0, 3, 0, 4, 0, 5, 0, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3,
+            4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,
+            2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    #[cfg(feature = "unix_timestamp")]
+    fn it_parses_v5_timestamp() {
+        let packet = [
+            0, 5, 2, 0, 3, 0, 4, 0, 5, 0, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3,
+            4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,
+            2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_creates_error() {
+        let packet = [12, 13, 14];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    #[cfg(not(feature = "unix_timestamp"))]
+    fn it_parses_v7() {
+        let packet = [
+            0, 7, 2, 0, 3, 0, 4, 0, 5, 0, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3,
+            4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,
+            2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    #[cfg(feature = "unix_timestamp")]
+    fn it_parses_v7_timestamp() {
+        let packet = [
+            0, 7, 2, 0, 3, 0, 4, 0, 5, 0, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3,
+            4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,
+            2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_v9() {
+        let packet = [
+            0, 9, 0, 2, 0, 0, 9, 9, 0, 1, 2, 3, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 16, 1, 2, 0,
+            2, 0, 1, 0, 4, 0, 8, 0, 4, 1, 2, 0, 12, 9, 2, 3, 4, 9, 9, 9, 8,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_v9_many_flows() {
+        let packet = [
+            0, 9, 0, 3, 0, 0, 9, 9, 0, 1, 2, 3, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 16, 1, 2, 0,
+            2, 0, 1, 0, 4, 0, 8, 0, 4, 1, 2, 0, 12, 9, 2, 3, 4, 9, 9, 9, 8, 9, 2, 3, 4, 9, 9,
+            9, 8,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_v9_options_template() {
+        let packet = [
+            0, 9, 0, 2, 0, 0, 9, 9, 0, 1, 2, 3, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 22, 1, 19, 0,
+            4, 0, 8, 0, 2, 0, 2, 0, 34, 0, 2, 0, 36, 0, 1, 1, 19, 0, 9, 0, 2, 0, 100, 1, 0,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_v9_data_cached_template() {
+        let packet = [
+            0, 9, 0, 1, 0, 0, 9, 9, 0, 1, 2, 3, 0, 0, 0, 1, 0, 0, 0, 1, 1, 2, 0, 12, 9, 2, 3,
+            4, 9, 9, 9, 8,
+        ];
+        let fields = vec![
+            V9TemplateField {
+                field_type_number: 1,
+                field_type: crate::variable_versions::v9_lookup::V9Field::InBytes,
+                field_length: 4,
+            },
+            V9TemplateField {
+                field_type_number: 8,
+                field_type: crate::variable_versions::v9_lookup::V9Field::Ipv4SrcAddr,
+                field_length: 4,
+            },
+        ];
+        let template = V9Template {
+            field_count: 2,
+            template_id: 258,
+            fields,
+        };
+        let mut parser = NetflowParser::default();
+        parser.v9_parser.templates.insert(258, template);
+        assert_yaml_snapshot!(parser.parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_ipfix() {
+        let packet = [
+            0, 10, 0, 64, 1, 2, 3, 4, 0, 0, 0, 0, 1, 2, 3, 4, 0, 2, 0, 20, 1, 0, 0, 3, 0, 8, 0,
+            4, 0, 12, 0, 4, 0, 2, 0, 4, 1, 0, 0, 28, 1, 2, 3, 4, 1, 2, 3, 3, 1, 2, 3, 2, 0, 2,
+            0, 2, 0, 1, 2, 3, 4, 5, 6, 7,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_doesnt_panic_with_invalid_options_ipfix_template() {
+        let packet = [
+            0, 10, 0, 44, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 3, 0, 28, 1, 4, 0, 13, 0, 1,
+            128, 123, 0, 4, 0, 0, 0, 2, 0, 41, 0, 2, 0, 42, 0, 2, 0, 0,
+        ];
+        NetflowParser::default().parse_bytes(&packet);
+    }
+
+    #[test]
+    fn it_parses_ipfix_data_cached_template() {
+        let packet = [
+            0, 10, 0, 26, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2, 0, 10, 0, 8, 0, 0, 1, 1,
+        ];
+        let fields = vec![
+            IPFixTemplateField {
+                field_type_number: 2,
+                field_type:
+                    crate::variable_versions::ipfix_lookup::IPFixField::PacketDeltaCount,
+                field_length: 2,
+                enterprise_number: None,
+            },
+            IPFixTemplateField {
+                field_type_number: 8,
+                field_type:
+                    crate::variable_versions::ipfix_lookup::IPFixField::SourceIpv4address,
+                field_length: 4,
+                enterprise_number: None,
+            },
+        ];
+        let template = IPFixTemplate {
+            field_count: 2,
+            template_id: 258,
+            fields,
+        };
+        let mut parser = NetflowParser::default();
+        parser.ipfix_parser.templates.insert(258, template);
+        assert_yaml_snapshot!(parser.parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_ipfix_with_no_template_fields_raises_error() {
+        let packet = [
+            0, 10, 0, 26, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2, 0, 10, 0, 8, 0, 0, 1, 1,
+        ];
+        let template = IPFixTemplate {
+            field_count: 2,
+            template_id: 258,
+            fields: vec![],
+        };
+        let mut parser = NetflowParser::default();
+        parser.ipfix_parser.templates.insert(258, template);
+        assert_yaml_snapshot!(parser.parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_v9_with_no_template_fields_raises_error() {
+        let packet = [
+            0, 9, 0, 26, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 2, 0, 10, 0, 8, 0, 0, 1, 1,
+        ];
+        let template = V9Template {
+            field_count: 2,
+            template_id: 258,
+            fields: vec![],
+        };
+        let mut parser = NetflowParser::default();
+        parser.v9_parser.templates.insert(258, template);
+        assert_yaml_snapshot!(parser.parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_ipfix_options_template() {
+        let packet = [
+            0, 10, 0, 44, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 3, 0, 28, 1, 4, 0, 3, 0, 1,
+            128, 123, 0, 4, 0, 0, 0, 2, 0, 41, 0, 2, 0, 42, 0, 2, 0, 0,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+
+    #[test]
+    fn it_parses_ipfix_options_template_with_data() {
+        let packet = [
+            0, 10, 0, 64, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 3, 0, 28, 1, 4, 0, 3, 0, 1,
+            128, 123, 0, 4, 0, 0, 0, 2, 0, 41, 0, 2, 0, 42, 0, 2, 0, 0, 1, 4, 0, 20, 0, 0, 0,
+            1, 1, 20, 20, 20, 0, 0, 0, 2, 20, 20, 30, 30,
+        ];
+        assert_yaml_snapshot!(NetflowParser::default().parse_bytes(&packet));
+    }
+}
