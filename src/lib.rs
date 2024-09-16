202e75c9a308d554c0e8b44cac4be317971a17db
mikemiles-dev <michaelmileusnich@Michaels-MacBook-Air-2.local>
2024-09-16 15:56:46 -0500

    Lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
 //! # netflow_parser
 //!
-//! A netflow_parser library for Cisco V5, V7, V9, IPFIX written in Rust.
+//! A Netflow Parser library for Cisco V5, V7, V9, IPFIX written in Rust.
 //! Supports chaining of multple versions in the same stream.  ({v5 packet}, {v7packet}, {v5packet}, {v9packet}, etc.)
 //!
 //! # References
 //! See: <https://en.wikipedia.org/wiki/NetFlow>
 //!
-//! # Example:
+//! # Description
 //!
-//! ## V5:
+//! ## Example
+//!
+//! ### V5
 //!
 //! ```rust
 //! use netflow_parser::{NetflowParser, NetflowPacket};
@@ -30,8 +32,6 @@
 //! let v5_packet = [0, 5, 2, 0, 3, 0, 4, 0, 5, 0, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7,];
 //! println!("{}", json!(NetflowParser::default().parse_bytes(&v5_packet)).to_string());
 //! ```
-//!
-//! ## Output:
 //!
 //! ```json
 //! [{"V5":{"body":{"d_octets":66051,"d_pkts":101124105,"dst_addr":"4.5.6.7","dst_as":515,"dst_mask":5,"dst_port":1029,"first":67438087,"input":515,"last":134807553,"next_hop":"8.9.0.1","output":1029,"pad1":6,"pad2":1543,"protocol":"EGP","src_addr":"0.1.2.3","src_as":1,"src_mask":4,"src_port":515,"tcp_flags":7,"tos":9},"header":{"count":512,"engine_id":7,"engine_type":6,"flow_sequence":33752069,"sampling_interval":2057,"sys_up_time":50332672,"unix_nsecs":134807553,"unix_secs":83887623,"unix_time":{"nanos_since_epoch":134807553,"secs_since_epoch":83887623},"version":5}}}]
